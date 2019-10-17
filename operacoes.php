91535fa65cdda378f452387a2f10f1cdcd2b01dd
Rafael Bergsten <rafael@techinf.com.br>
2019-10-17 10:52:05 -0300

    add mathematical operation

---
+++ operacoes.php
@@ -0,0 +1,13 @@
+<?php 
+
+$idadeAtual = 32;
+$idadeDaqui10Anos = $idadeAtual + 10;
+
+$soma = 2 + 2;
+echo $soma;
+$subtracao = 2 - 2;
+echo $subtracao;
+$divisao = 4/2;
+echo $divisao;
+$multiplicacao = 2 * 2;
+echo $multiplicacao;
