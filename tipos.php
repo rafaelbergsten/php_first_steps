96a9f8dd1e7a739a42e5e048407544470b1bf82b
Rafael Bergsten <rafael@techinf.com.br>
2019-10-17 11:07:02 -0300

    add files types php

---
+++ tipos.php
@@ -0,0 +1,14 @@
+<?php 
+
+$idade = 32;
+$salario = 1000.30;
+$verdadeiro = true;
+$falso = false;
+
+echo gettype($idade);
+echo gettype($salario);
+echo gettype($verdadeiro);
+echo gettype($falso);
+
+//Primites types in PHP = int, decimal, boolean, string
+
