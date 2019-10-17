376b1bb53d77c2c27426f79dd28c0126bd18df1e
Rafael Bergsten <rafael@techinf.com.br>
2019-10-17 17:36:52 -0300

    add file repetion

---
+++ repeticao.php
@@ -0,0 +1,15 @@
+<?php 
+
+$contador = 1;
+
+while ($contador <= 15) {
+    echo "#$contador" . PHP_EOL;
+    $contador = $contador + 1;
+}
+
+for ($contador = 1; $contador <= 15; $contador ++) {
+    echo "#$contador" . PHP_EOL;
+}
+
+for ($contador = 1; $contador <= 15; $contador ++) 
+    echo "#$contador" . PHP_EOL;
