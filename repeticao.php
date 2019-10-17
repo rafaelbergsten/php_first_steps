5c70db0ab9fad1574a4f39d82b2d8be6a43924bb
Rafael Bergsten <rafael@techinf.com.br>
2019-10-17 17:40:23 -0300

    add break and continue

--- repeticao.php
+++ repeticao.php
@@ -13,3 +13,17 @@ for ($contador = 1; $contador <= 15; $contador ++) {
 
 for ($contador = 1; $contador <= 15; $contador ++) 
     echo "#$contador" . PHP_EOL;
+
+for ($contador = 1; $contador <= 15; $contador ++) {
+    if ($contador == 13) {
+        continue;
+    } 
+    echo "#$contador" . PHP_EOL;
+}
+
+for ($contador = 1; $contador <= 15; $contador ++) {
+    if ($contador == 13) {
+        break;
+    } 
+    echo "#$contador" . PHP_EOL;
+}
