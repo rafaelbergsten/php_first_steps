c4617310da3df1fd17a588ef796f3c01bf8c1eaa
Rafael Bergsten <rafael@techinf.com.br>
2019-10-17 17:47:03 -0300

    add a file desafios

---
+++ desafios.php
@@ -0,0 +1,10 @@
+<?php
+/*
+    Escrever um programa que imprima de 0 a 100 os numeros primos
+*/
+
+for ($contador = 0; $contador <=100; $contador ++){
+    if($contador%2!=0){
+        echo "o numero e impar $contador";
+    }
+}
